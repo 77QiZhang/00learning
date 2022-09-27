c0c5db2d8fc194e0d4adc4d15fea888903d08f43
77QiZhang <zqqchina@163.com>
2022-09-27 20:11:04 +0800

    run right

--- nnunet_strix.py
+++ nnunet_strix.py
@@ -1,37 +1,32 @@
-#相对路径转绝对路径
-from cProfile import label
-from email.mime import image
-
-import os
 import json
-from pathlib import Path
-from random import shuffle
-import nibabel as nib
-
+import os
 #先从nnunet的data.json中提取路径
 #转绝对路径
 #整合
 
 def get_nnunet_case_files(nnUNet , dataset = None):
-    nnUNet["training"]=json.loads(nnUNet["training"])#把json格式转为python识别的格式
-    training_list = nnUNet["training"]#是个列表
+    with open(nnUNet) as json_file:
+        nnUNet_data = json.load(json_file)#成功修复第一个bug！！！！！！！！！
 
-    num = len(nnUNet["training"])
+    #nnUNet["training"]=json.loads(nnUNet["training"])#把json格式转为python识别的格式
+    training_list = nnUNet_data["training"]#是个列表
+
+    num = len(nnUNet_data["training"])
     dataset = []
     i = 0#存疑
 #建立一个列表，列表的第几位是一个字典
-    for i in num:
+    for i in range(num):
         case_data = {}
         image_relpath = training_list[i]["image"]
         label_relpath = training_list[i]["label"]
-        mask_relpath = training_list[i]["mask"]
+        #mask_relpath = training_list[i]["mask"]#此处应加判断
         case_data['image'] = os.path.abspath(image_relpath)
         case_data['label'] = os.path.abspath(label_relpath)
-        case_data['mask'] = os.path.abspath(mask_relpath)
+        #case_data['mask'] = os.path.abspath(mask_relpath)
         dataset.append(case_data)
         i = i + 1
         #相对路径转绝对路径os.path.abspath(path)
     print(dataset)
 
-nnUNet = r'C:\Users\37225\Documents\WeChat Files\wxid_fj1t1hokcu8h12\FileStorage\File\2022-09\dataset.json'
+nnUNet = r'C:\Users\Qi Zhang\Documents\WeChat Files\wxid_fj1t1hokcu8h12\FileStorage\MsgAttach\48349edd9a01ff35af8f80b8a71854d9\File\2022-09\dataset.json'
 get_nnunet_case_files(nnUNet)
